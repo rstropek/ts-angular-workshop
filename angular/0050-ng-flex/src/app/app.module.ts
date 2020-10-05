528b52bfbab3dfe5e66bc6637bbba33f6b12c950
Rainer Stropek <rainer@software-architects.at>
2020-10-05 08:31:29 +0200

    Update Angular Flex sample to Angular 10

--- angular/0050-ng-flex/src/app/app.module.ts
+++ angular/0050-ng-flex/src/app/app.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { SimpleLayoutsComponent } from './simple-layouts/simple-layouts.component';
 import { OrderingComponent } from './ordering/ordering.component';
-
 
 @NgModule({
   declarations: [
